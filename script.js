dbc2e16695f3540501e0133a70e3897c16e1fbc6
Akshara Tanted <ranitanted0676@gmail.com>
2026-02-06 11:55:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function playMusic() {
+    document.getElementById("bgMusic").play();
+}
+
+function showVision() {
+    document.getElementById("visionBoard").style.display = "flex";
+}
+
+function closeVision() {
+    document.getElementById("visionBoard").style.display = "none";
+}
